565aafb3a22f5d29916e63f750c4891e99f0f75e
DESKTOP-EK3791T\RAMLINA <mino.fadio69@gmail.com>
2025-04-27 23:24:06 +0100

    first commit for copilotov 2

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,3 +1,6 @@
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatelessWidget {
@@ -7,4 +10,10 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(appBar: AppBar(backgroundColor: Colors.blue));
   }
+}
+
+getProducts() async {
+  final Dio dio = Dio();
+  final Response response = await dio.get('https://dummyjson.com/products/1');
+  log("The response data is ${response.data}");
 }
